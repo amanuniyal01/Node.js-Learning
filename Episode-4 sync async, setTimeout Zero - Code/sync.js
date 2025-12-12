470ab8788dc7e4d0802348c041a786705bed7715
amanuniyal01 <amanuniyal315@gmail.com>
2025-12-12 15:22:08 +0530

    Completed episode 4 of my node.js learning

---
+++ Episode-4 sync async, setTimeout Zero - Code/sync.js
@@ -0,0 +1,8 @@
+var a = 102346;
+var b = 535231;
+function multiplyFn(x, y) {
+    const result = x * y
+    return result
+}
+var c = multiplyFn(a, b)
+console.log(`Multiplication of ${a} and ${b} is : ${c}`)
