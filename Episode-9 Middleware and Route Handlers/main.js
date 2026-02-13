d1b0036e3085525cd3ac3d62f7f96bb4d28878d9
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-13 10:11:06 +0530

    Completed episode 9 with great knowledge

--- Episode-9 Middleware and Route Handlers/main.js
+++ Episode-9 Middleware and Route Handlers/main.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
 
-const auth = require("./auth");
+// const auth = require("./auth");
 //  Middleware for ALL /user requests (all HTTP methods)
-app.use("/user", auth);
+// app.use("/user", auth);
 
 
 //  AFTER middleware
-app.get("/user/login", (req, res) => {
-    res.send("User Logged In");
-});
+// app.get("/user/login", (req, res) => {
+//     res.send("User Logged In");
+// });
 
-app.get("/user", (req, res) => {
-    res.send("User Home");
-});
+// app.get("/user", (req, res) => {
+//     res.send("User Home");
+// });
 
 app.get("/user/details", (req, res) => {
-    res.send("User Details");
+    throw new Error("abcdef")
+    res.send("User Details sent successfully");
 });
+// Error should be the first parameter then will only it works.
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong")
+    }
 
+})
 
 
 app.listen(3000, () => {
